bd42b92e5fdb71d28c340d96ddc98eac1049482c
Антонина Пимонова <ae.pimonova@gmail.com>
2022-10-17 16:09:55 +0300

    Добавьте файлы проекта.

---
+++ Weather/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите показания температуры");
+            int[] temperature = new int [1];
+            int i = 0;
+            while(int.TryParse(Console.ReadLine(), out temperature[i]))
+            {
+                Array.Resize(ref temperature, temperature.Length + 1);
+                i++;
+            }
+            Console.Clear();
+            for (int k = 0; k < temperature.Length; k++)
+            {
+                if (temperature[k] > 0)
+                {
+                    Console.WriteLine($"{k}. (+){temperature[k]}");
+                }
+
+                if (temperature[k] < 0)
+                {
+                    Console.WriteLine($"{k}. (-){Math.Abs(temperature[k])}");
+                }
+
+            }
+        }
+    }
+}
